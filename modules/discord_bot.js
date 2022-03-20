31b83af7de0080e409cff7004f0c138a1982b1d2
Sad <cougar.l@hotmail.com>
2022-03-21 04:18:01 +0800

    Update discord_bot.js

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -862,7 +862,7 @@ client.on('messageReactionRemove', async (reaction, user) => {
 
 
 function z_stop(mainMsg, groupid) {
-	if (!Object.keys(exports.z_stop).length || !exports.z_stop.initialize().save) {
+	if ((exports.z_stop && !Object.keys(exports.z_stop).length) || !exports.z_stop.initialize().save) {
 		return false;
 	}
 	let groupInfo = exports.z_stop.initialize().save.find(e => e.groupid == groupid)
